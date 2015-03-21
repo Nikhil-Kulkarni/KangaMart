34b99f2e715d077caae8908abd747ea3eebc63fa
sivam <smanivasagam3@gatech.edu>
2015-03-21 12:29:39 -0400

    Buttons for selection.xml

--- eMartAndroid/KangaMart/app/src/main/java/com/example/brandon/kangamart/Selection.java
+++ eMartAndroid/KangaMart/app/src/main/java/com/example/brandon/kangamart/Selection.java
@@ -4,14 +4,35 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 
 public class Selection extends ActionBarActivity {
-
+    Button order;
+    Button delivery;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selection);
+        order = (Button) findViewById(R.id.Order);
+        order.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Selection.this, Order.class);
+                startActivity(intent);
+            }
+        });
+        delivery = (Button) findViewById(R.id.Order);
+        delivery.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Selection.this, Delivery.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 
